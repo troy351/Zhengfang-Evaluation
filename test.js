c64468d2765e0cf7509e5b816ed524c8aa3805b7
troy <914053923@qq.com>
2015-12-15 15:09:31 +0800

    decrease code

--- test.js
+++ test.js
@@ -2,13 +2,14 @@ var courseCount = 5;
 for (var j = 1; j <= courseCount; j++) {
     setTimeout(function () {
         var random = Math.ceil(Math.random() * 11) + 1;
+        var frameDocument = document.getElementById('iframeautoheight').contentWindow.document;
         for (var i = 2; i <= 12; i++) {
-            document.getElementById('iframeautoheight').contentWindow.document.getElementById('DataGrid1__ctl' + i + '_JS1').selectedIndex = random == i ? 2 : 1;
-            var ctl2 = document.getElementById('iframeautoheight').contentWindow.document.getElementById('DataGrid1__ctl' + i + '_JS2');
+            frameDocument.getElementById('DataGrid1__ctl' + i + '_JS1').selectedIndex = random == i ? 2 : 1;
+            var ctl2 = frameDocument.getElementById('DataGrid1__ctl' + i + '_JS2');
             if (ctl2) {
                 ctl2.selectedIndex = random == i ? 2 : 1;
             }
         }
-        document.getElementById('iframeautoheight').contentWindow.document.getElementById('Button1').click();
+        frameDocument.getElementById('Button1').click();
     }, j * 1000);
 }
